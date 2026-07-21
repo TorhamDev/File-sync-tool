cb687510c2e7e9b6502ab9da2b52cdca9c2c7c77
torham <torhamdev@gmail.com>
2026-07-21 20:33:46 +0330

    Move dabase models into models.py

---
+++ src/database/models.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import Enum as SqlEnum
+
+from database.db import Base
+
+
+class SyncTypeEnum(Enum):
+    TELEGRAM = "Telegram"
+    GITHUB = "Github"
+    GDRIVE = "GoogleDrive"
+
+
+class BaseInfo(Base):
+    __tablename__ = "base_info"
+    info_id = Column(Integer, primary_key=True)
+    sync_type = Column(SqlEnum(SyncTypeEnum), nullable=False)
+
+
+class TelegramInfo(Base):
+    __tablename__ = "telegram_info"
+    tel_info_id = Column(Integer, primary_key=True)
+    token = Column(String)
+    channel_id = Column(String)
+
+
+class GithubInfo(Base):
+    __tablename__ = "github_info"
+    github_info_id = Column(Integer, primary_key=True)
+    token = Column(String)
+    repo_owner = Column(String)
+    repo_name = Column(String)
+
+
+class GoogleDriveInfo(Base):
+    __tablename__ = "gdrive_info"
+    gdrive_info_id = Column(Integer, primary_key=True)
+    credentials_json = Column(String)
+    target_folder = Column(String)
