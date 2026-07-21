cda43f3d986df7471279bb792fc1fa9909cd8d0c
torham <torhamdev@gmail.com>
2026-07-21 21:48:21 +0330

    Add starter funtion and telegram starter funtion

---
+++ src/utils/starters.py
@@ -0,0 +1,30 @@
+from consts import TELEGRAM_BOT_TOKEN_REGEX, PlatfromsTypeEnum
+from utils.validations import regex_validation
+
+
+def telegram_starter() -> tuple[str, str]:
+    while True:
+        bot_token = input("Enter Your Bot Token: ")
+        if not regex_validation(pattern=TELEGRAM_BOT_TOKEN_REGEX, string=bot_token):
+            continue
+        channel_id = input("Enter Your Telegram Channel ID: ")
+        return bot_token, channel_id
+
+
+def select_platfrom() -> str:
+    print("please select your sync platfrom:")
+
+    platforms = [e.value for e in PlatfromsTypeEnum]
+
+    for number, name in enumerate(platforms):
+        print(f"{number}. {name}")
+
+    while True:
+        try:
+            platfrom = int(input(f"Enter Platfrom Number(0-{len(platforms) - 1}): "))
+
+            return platforms[platfrom]
+        except ValueError:
+            print(f"Please Enter a valid number (0-{len(platforms) - 1})")
+        except IndexError:
+            print(f"please select from  (0-{len(platforms) - 1})")
