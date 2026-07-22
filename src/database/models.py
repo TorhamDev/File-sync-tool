817e797171d5c8ca6bc829a9015075d76a7a6b57
torham <torhamdev@gmail.com>
2026-07-22 21:04:17 +0330

    Change models id column name  to single word

--- src/database/models.py
+++ src/database/models.py
@@ -7,20 +7,20 @@ from database.db import Base
 
 class BaseInfo(Base):
     __tablename__ = "base_info"
-    info_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     sync_type = Column(SqlEnum(PlatfromsTypeEnum), nullable=False)
 
 
 class TelegramInfo(Base):
     __tablename__ = "telegram_info"
-    tel_info_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     token = Column(String)
     channel_id = Column(String)
 
 
 class GithubInfo(Base):
     __tablename__ = "github_info"
-    github_info_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     token = Column(String)
     repo_owner = Column(String)
     repo_name = Column(String)
@@ -28,6 +28,6 @@ class GithubInfo(Base):
 
 class GoogleDriveInfo(Base):
     __tablename__ = "gdrive_info"
-    gdrive_info_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     credentials_json = Column(String)
     target_folder = Column(String)
