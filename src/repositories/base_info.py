8b2b6c58b91d05a8319038b0f87587f50dcb47cc
torham <torhamdev@gmail.com>
2026-07-22 21:07:39 +0330

    Add BaseInfoRepository

---
+++ src/repositories/base_info.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+
+from consts import PlatfromsTypeEnum
+from database.models import BaseInfo
+
+
+class BaseInfoRepository:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def base_info_exists(self) -> bool:
+        query = self.session.query(BaseInfo).order_by(BaseInfo.id.desc())
+        return self.session.query(query.exists()).scalar()
+
+    def get_last_base_info(self) -> BaseInfo | None:
+        return self.session.query(BaseInfo).order_by(BaseInfo.id.desc()).first()
+
+    def create_base_info(self, sync_type: PlatfromsTypeEnum) -> BaseInfo:
+        query = BaseInfo(sync_type=sync_type)
+        self.session.add(query)
+        return self.get_last_base_info()
