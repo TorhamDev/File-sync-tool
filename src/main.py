fc1e10b004a19f88d2bae6e3ca13d4cccde6fb4f
torham <torhamdev@gmail.com>
2026-07-22 21:08:12 +0330

    Add basic program starter

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 from database import get_db
+from repositories.base_info import BaseInfoRepository
+from utils.starters import get_platfrom_starter, select_platfrom
+
 if __name__ == "__main__":
-    print(get_db())
+    base_info_repo = BaseInfoRepository(session=get_db())
+
+    if base_info_repo.base_info_exists():
+        pass
+
+    else:
+        platfrom_starter = get_platfrom_starter(select_platfrom())
+
+        platfrom_starter()
