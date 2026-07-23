5dd6671c6bdd61a7b9c46af9bf42837b160255a3
torham <torhamdev@gmail.com>
2026-07-23 19:46:05 +0330

    Add create base info in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from database import get_db
 from repositories.base_info import BaseInfoRepository
 from utils.starters import get_platfrom_starter, select_platfrom
+from consts import PlatfromsTypeEnum
 
 if __name__ == "__main__":
     base_info_repo = BaseInfoRepository(session=get_db())
@@ -9,6 +10,11 @@ if __name__ == "__main__":
         pass
 
     else:
-        platfrom_starter = get_platfrom_starter(select_platfrom())
-
+        platfrom = select_platfrom()
+        platfrom_starter = get_platfrom_starter(platfrom)
+        sync_dir = input("Enter Target dir you want to sync: ")
+        base_info = base_info_repo.create_base_info(
+            sync_type=PlatfromsTypeEnum(platfrom),
+            sync_dir=sync_dir,
+        )
         platfrom_starter()
