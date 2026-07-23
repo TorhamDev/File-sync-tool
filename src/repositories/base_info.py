82c9b086a9e84550078312ce39c340818249e851
torham <torhamdev@gmail.com>
2026-07-23 19:45:20 +0330

    Add create_base_info

--- src/repositories/base_info.py
+++ src/repositories/base_info.py
@@ -15,7 +15,7 @@ class BaseInfoRepository:
     def get_last_base_info(self) -> BaseInfo | None:
         return self.session.query(BaseInfo).order_by(BaseInfo.id.desc()).first()
 
-    def create_base_info(self, sync_type: PlatfromsTypeEnum) -> BaseInfo:
-        query = BaseInfo(sync_type=sync_type)
+    def create_base_info(self, sync_type: PlatfromsTypeEnum, sync_dir: str) -> BaseInfo:
+        query = BaseInfo(sync_type=sync_type, sync_dir=sync_dir)
         self.session.add(query)
         return self.get_last_base_info()
