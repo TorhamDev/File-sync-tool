b113423386794dc020d94f753696df955d83d456
torham <torhamdev@gmail.com>
2026-07-23 19:45:31 +0330

    Add telegram repository

---
+++ src/repositories/telegram.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+
+from database.models import TelegramInfo
+
+
+class TelegramRepository:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def get_telegram_info_by_channel_id(self, channel_id: str) -> TelegramInfo:
+        query = self.session.query(TelegramInfo).where(
+            TelegramInfo.channel_id == channel_id
+        )
+        return query.scalar()
+
+    def create_telegram_info(self, token: str, channel_id: str) -> TelegramInfo:
+        query = TelegramInfo(token=token, channel_id=channel_id)
+        self.session.add(query)
+        return self.get_telegram_info_by_channel_id(channel_id=channel_id)
