c96176ab43fb07c2f7847f5478c50c0e2b482ebe
torham <torhamdev@gmail.com>
2026-07-24 20:56:51 +0330

    Improve regex_validation function

--- src/utils/validations.py
+++ src/utils/validations.py
@@ -2,8 +2,4 @@ import re
 
 
 def regex_validation(pattern: str, string: str) -> bool:
-    match = re.match(pattern=pattern, string=string)
-    if match:
-        return True
-
-    return False
+    return bool(re.match(pattern=pattern, string=string))
