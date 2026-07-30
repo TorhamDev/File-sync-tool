fa9d803501ac025a1c56d25d75547c9e4c17d57f
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2026-07-30 22:31:35 +0330

    docs: add printing help text

--- src/platforms/telegram/starter.py
+++ src/platforms/telegram/starter.py
@@ -3,8 +3,13 @@ from database.db import get_db
 from repositories.telegram import TelegramRepository
 from utils.validations import regex_validation
 
+HELP_TEXT = """
+insure to have a channel and a bot, and add the bot to your channel as an admin.
+get your bot token from @BotFather and your channel id from @username_to_id_bot
+"""
 
 def telegram_starter() -> tuple[str, str]:
+    print(HELP_TEXT)
     while True:
         bot_token = input("Enter Your Bot Token: ")
         if not regex_validation(pattern=TELEGRAM_BOT_TOKEN_REGEX, string=bot_token):
